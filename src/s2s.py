5d466daa17b15b9b19cc1394ed539561c3c708d8
ShuoW <ws610830028@gmail.com>
2016-09-07 23:36:14 +0100

    simple model

---
+++ src/s2s.py
@@ -0,0 +1,133 @@
+from keras.layers import Activation, TimeDistributed, Dense, RepeatVector, recurrent
+from keras.models import Sequential
+import random
+from ulti import *
+
+# set the training dataset
+set_name = 'train5'
+qvalid_data = read_data(set_name)
+
+# put the data into a list one by one
+text = []
+for i in range(0,len(qvalid_data)):
+    for j in range(0, 20):
+        text.append(qvalid_data[i][j])
+
+# the total word number
+print 'corpus length:', len(text)
+# the total vocabulary size
+chars = set(text)
+print 'total chars:', len(chars)
+
+# build the dictionary
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# the length of a line
+maxlen = 5
+# the length of a poetry
+poetry_len = 20
+
+# the input sentence/next sentence
+sentences = []
+next_sentences = []
+
+# put the content to the sentence/next sentence
+for i in range(0, len(qvalid_data)):
+    for j in range(0, poetry_len - maxlen * 2 + 1, maxlen):
+        sentences.append(qvalid_data[i][j:j+maxlen])
+        next_sentences.append(qvalid_data[i][j + maxlen: j + maxlen * 2])
+# the number of the sentence/next sentence pair
+print 'nb sequences:', len(sentences)
+
+# vectorization
+print 'Vectorization...'
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(next_sentences), maxlen, len(chars)), dtype=np.bool)
+
+# build the one-hot model
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+
+for i, sentence in enumerate(next_sentences):
+    for t, char in enumerate(sentence):
+        y[i, t, char_indices[char]] = 1
+
+# build the model: simple sequence to sequence model
+print 'Build model...'
+
+# the hidden unit number
+hidden_dim = 100
+# the layer number
+layer = 3
+# the rnn element
+RNN = recurrent.LSTM
+
+
+model = Sequential()
+# The encoder transfers the sentence to the hidden layer
+model.add(RNN(hidden_dim, input_shape=(maxlen, len(chars))))
+# For the decoder's input, we repeat the encoded input for each time step
+model.add(RepeatVector(maxlen))
+# The decoder RNN could be multiple layers stacked or a single layer
+for _ in range(layer):
+    model.add(RNN(hidden_dim, return_sequences=True))
+# For each of step of the output sequence, decide which character should be chosen
+model.add(TimeDistributed(Dense(len(chars))))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=['accuracy'])
+
+# print the model information
+print model.summary()
+out = codecs.open("../data/result_s2s", 'w', 'utf-8')
+# train the model, output generated text after each iteration
+for iteration in range(1, 100):
+    print '-' * 50
+    print 'Iteration', iteration
+
+    # put the data into the model
+    model.fit(X, y, batch_size=64, nb_epoch=50)
+
+    # random select the first line
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    start_index = start_index / 20 * 20
+
+    # add the diversity parameter in the sample function
+    # for diversity in [0.2, 0.5, 1.0, 1.2]:
+    diversity = 1.0
+    print '----- diversity:', diversity
+    # selecting the first line
+    sentence = text[start_index: start_index + maxlen]
+    # write down the first line to the result file
+    out.write("".join(sentence))
+
+    print '----- Generating with seed: "' + ''.join(sentence).encode('utf-8') + '"'
+
+    # run 3 time to generate 3 lines
+    for i in range(0, 3):
+
+        # build the one hot model of the seed sentence
+        x = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x[0, t, char_indices[char]] = 1.
+
+        # predict with the next line
+        preds = model.predict(x, verbose=0)[0]
+        # initial for the new sentence
+        new_sentence = []
+
+        # print each character
+        for j in range(0, 5):
+            next_index = sample(preds[j], diversity)
+            next_char = indices_char[next_index]
+            new_sentence.append(next_char)
+
+        sentence = new_sentence
+        out.write("".join(sentence))
+        print '----- Generate: "' + ''.join(new_sentence).encode('utf-8') + '"'
+    out.write("\n")
+out.flush()
+out.close()
+
+
---
+++ src/w2w.py
@@ -0,0 +1,98 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.layers import LSTM
+import random
+from ulti import *
+
+
+# the print function
+def print_g(g):
+    full = ''.join(g)
+    print full[0:5]
+    print full[5:10]
+    print full[10:15]
+    print full[15:20]
+
+# set the training dataset
+set_name = 'train5'
+qvalid_data = read_data(set_name)
+
+text = []
+for i in range(0,len(qvalid_data)):
+    for j in range(0, 20):
+        text.append(qvalid_data[i][j])
+print('corpus length:', len(text))
+
+# build the dictionary
+chars = set(text)
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# build the sentence
+maxlen = 5
+step = 1
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('nb sequences:', len(sentences))
+
+# build the vector
+print('Vectorization...')
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+# build the model: two LSTM layers
+print('Build model...')
+model = Sequential()
+model.add(LSTM(32, return_sequences=True, input_shape=(maxlen, len(chars))))
+model.add(Dropout(0.2))
+model.add(LSTM(32, return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+
+# train the model, output generated text after each iteration with the different diversity
+for iteration in range(1, 100):
+    print()
+    print('-' * 50)
+    print('Iteration', iteration)
+    model.fit(X, y, batch_size=128, nb_epoch=50)
+
+    # select the seed sentence
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    start_index = start_index / 20 * 20
+
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print('----- diversity:', diversity)
+
+        generated = []
+        sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + ''.join(sentence) + '"')
+
+        # predict the characters
+        for i in range(15):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x, verbose=0)[0]
+            # predict the next character
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+            generated += next_char
+            # compose the sentence
+            sentence = sentence[1:] + [next_char]
+        print_g(generated)
+
+
